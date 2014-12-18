20f8aa3730af22177a8b06d55f048e503b66279d
richardDelsink <rd.richard@hotmail.com>
2014-12-18 17:07:44 +0100

    update login
    login class id toegevoegd om de classes in de css file op te roepen

--- Production/ICT4EVENTS/ICT4EVENTS/Login.aspx.designer.cs
+++ Production/ICT4EVENTS/ICT4EVENTS/Login.aspx.designer.cs
@@ -11,5 +11,23 @@ namespace ICT4EVENTS {
     
     
     public partial class Login {
+        
+        /// <summary>
+        /// sign control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm sign;
+        
+        /// <summary>
+        /// b1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button b1;
     }
 }
