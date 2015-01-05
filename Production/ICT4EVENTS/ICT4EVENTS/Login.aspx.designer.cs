2ae83ffc670b859879241d8896c8e658e9f753bd
Dennisoost <d.oosterling@student.fontys.nl>
2015-01-05 11:14:02 +0100

    Fixed login error with double pages (Merge erro)

--- Production/ICT4EVENTS/ICT4EVENTS/Login.aspx.designer.cs
+++ Production/ICT4EVENTS/ICT4EVENTS/Login.aspx.designer.cs
@@ -92,23 +92,5 @@ namespace ICT4EVENTS {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnSignup;
-        
-        /// <summary>
-        /// sign control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm sign;
-        
-        /// <summary>
-        /// b1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button b1;
     }
 }
