1c29e339532f96fb878226147cfd2fc796a948cb
AsrorWali <sardor-g@hotmail.com>
2015-01-05 15:35:39 +0100

    Revert "Events Createn en Viewen"
    This reverts commit 1595ba9c5e93e8831fa8f76dedbe1adf83f69f17.

--- Production/ICT4EVENTS/ICT4EVENTS/Events.aspx
+++ Production/ICT4EVENTS/ICT4EVENTS/Events.aspx
@@ -1,26 +1,9 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Events.aspx.cs" Inherits="ICT4EVENTS.WebForm1" %>
 
-
 <asp:Content ID="eventsContent" ContentPlaceHolderID="MainContent" runat="server">
-
-    <webopt:bundlereference runat="server" path="~/Content/Events.css" />
-
-    
     <div class="home_container">
        <div class ="content-body">
-           <div id="signin"><p>eyeyeyeyey</p></div>
         <p>INSERT YOU SHIT</p>
        </div>
-        
     </div>
-
-
-
-    
-    
-
-
 </asp:Content>
-
-
-    
--- Production/ICT4EVENTS/ICT4EVENTS/Events_admin.aspx.cs
+++ Production/ICT4EVENTS/ICT4EVENTS/Events_admin.aspx.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -10,25 +9,9 @@ namespace ICT4EVENTS
 {
     public partial class Events_admin : System.Web.UI.Page
     {
-        DatabaseConnection db = new DatabaseConnection();
-        DataSet ds = new DataSet();
         protected void Page_Load(object sender, EventArgs e)
         {
-            GridView1.DataSource = db.GetEvents();
-            GridView1.DataBind();
-            
-            
-        }
 
-        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            
-        }
-
-
-        protected void Button1_Click(object sender, EventArgs e)
-        {
-            
         }
     }
 }
