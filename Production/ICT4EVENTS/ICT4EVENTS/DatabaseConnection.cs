12bdbf1c12584bed7b01ebe6b7e2dcdd2ff3e0f5
JeffKain <Heartless275>
2015-01-05 09:29:56 +0100

    Update

--- Production/ICT4EVENTS/ICT4EVENTS/DatabaseConnection.cs
+++ Production/ICT4EVENTS/ICT4EVENTS/DatabaseConnection.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using Oracle.DataAccess.Client;
@@ -19,6 +20,41 @@ namespace ICT4EVENTS
             this.conn.ConnectionString = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
             this.conn.Open();
             this.conn.Close();
+        }
+
+        public int GetPlaces()
+        {
+
+            int uitkomst = 0;
+            int id;
+
+            OracleDataReader Reader;
+            this.conn = new OracleConnection();
+            this.conn.ConnectionString = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+            OracleCommand cmd = new OracleCommand("SELECT * from Plek", conn);
+            cmd.CommandType = CommandType.Text;
+            conn.Open();
+
+            Reader = cmd.ExecuteReader();
+
+            try
+            {
+                while (Reader.Read())
+                {
+                    uitkomst = Convert.ToInt16(Reader.GetValue(3));
+                }
+            }
+            catch (OracleException OE)
+            {
+
+            }
+            finally
+            {
+                conn.Close();
+
+            }
+            return uitkomst;
+
         }
     }
 }
