73521b4dbed9b8e7af8d6e6b7d2e34a124f7bbec
TimOomis <t.oomis@student.fontys.nl>
2015-01-08 14:39:45 +0100

    HomeSync

--- Production/ICT4EVENTS/ICT4EVENTS/Home.aspx.cs
+++ Production/ICT4EVENTS/ICT4EVENTS/Home.aspx.cs
@@ -4,14 +4,25 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Businesslayer;
 
 namespace ICT4EVENTS
 {
     public partial class _Default : Page
     {
+         Business B = new Business();
         protected void Page_Load(object sender, EventArgs e)
         {
+            FillListBoxes();
+        }
 
+        public void FillListBoxes()
+        {
+            foreach (string s in B.ReservationInfo("admin"))
+            {
+                ReserverationInfoListBox.Items.Add(s);
+            }
+           
         }
     }
 }
--- Production/ICT4EVENTS/ICT4EVENTS/Home.aspx.designer.cs
+++ Production/ICT4EVENTS/ICT4EVENTS/Home.aspx.designer.cs
@@ -11,5 +11,14 @@ namespace ICT4EVENTS {
     
     
     public partial class _Default {
+        
+        /// <summary>
+        /// ReserverationInfoListBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ListBox ReserverationInfoListBox;
     }
 }
