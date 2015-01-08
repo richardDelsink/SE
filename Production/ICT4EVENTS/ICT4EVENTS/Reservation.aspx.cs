99eac00030e8c2679ef2360113181ca5eda300eb
JeffKain <Heartless275>
2015-01-08 12:41:41 +0100

    Reservation system added
    Reservering wordt nog niet toegevoegd aan de database

--- Production/ICT4EVENTS/ICT4EVENTS/Reservation.aspx.cs
+++ Production/ICT4EVENTS/ICT4EVENTS/Reservation.aspx.cs
@@ -5,16 +5,52 @@ using System.Drawing;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Businesslayer;
+using ICT4EVENTS.Models;
 
 namespace ICT4EVENTS
 {
     public partial class Reservation : System.Web.UI.Page
     {
+        Reservering db = new Reservering();
+        ReservationRegister ResReg = new ReservationRegister();
+
+        private int selected = 0;
+
+        List<string> place = new List<string>();
+        private string color;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (Session["selected"] != null)
+            {
+                selected = (int) (Session["selected"]);
+            }
+            if (!this.IsPostBack)
+            {
+                selected = 0;
+            }
         }
 
+        protected void Getcolor(object sender, EventArgs e)
+        {
+            place = db.Getplaces();
+            Button btn = (Button) sender;
+            string number = btn.Text;
+            if (btn.BackColor != Color.Blue)
+            {
+                if (place.Contains(number))
+                {
+                    btn.BackColor = Color.Red;
+                    btn.Enabled = false;
+                }
+                else
+                {
+                    btn.BackColor = Color.Green;
+                    btn.Enabled = true;
+                }
+                
+            }
+        }
         protected void Btn_Click(object sender, System.EventArgs e)
         {
             Button btn = (Button)sender;
@@ -25,14 +61,54 @@ namespace ICT4EVENTS
                     break;
                 case "Blue":
                     btn.BackColor = Color.Green;
+                    Session["selected"] = 0;
+                    selected = 0;
                     break;
                 case "Green":
                     btn.BackColor = Color.Blue;
-                    break;
-                default:
-                    btn.BackColor = Color.Green;
+                    Session["selected"] = 1;
+                    color = "Test";
+
                     break;
             }
+        }
+
+        protected void BtnReserveer_Click(object sender, System.EventArgs e)
+        {
+            if (Page.IsValid)
+            {
+                ResReg.GetPersons(Convert.ToInt16(DropDownList1.SelectedValue));
+                Response.Redirect("ReservationRegister.aspx");
+            }
+        }
+
+        protected void CustomValidator1_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+
+            if (Calendar1.SelectedDate == null || Calendar1.SelectedDate == new DateTime(0001, 1, 1, 0, 0, 0) ||
+                Calendar2.SelectedDate == null || Calendar2.SelectedDate == new DateTime(0001, 1, 1, 0, 0, 0))
+            
+                // not click any date
+            {
+                args.IsValid = false;
+            }
+            else
+                args.IsValid = true;
+
+        }
+
+       protected void CustomValidator2_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+               if (selected ==0)
+
+                   // not click any date
+               {
+                   args.IsValid = false;
+               }
+               else
+               {
+                   args.IsValid = true;
+               }
         }
     }
 }
---
+++ Production/ICT4EVENTS/ICT4EVENTS/ReservationRegister.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ReservationRegister.aspx.cs" Inherits="ICT4EVENTS.ReservationRegister" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="~/Content/normalizer.css" rel="stylesheet" />
+    <link href="~/Content/Register.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+            <div class="signup">
+                <h1>Persons To Register : <asp:Label ID="LblRegister" runat="server" >5</asp:Label></h1>
+                <asp:TextBox ID="tbVoornaam" runat="server" Class="input" placeholder="Voornaam"></asp:TextBox>
+                                <asp:RequiredFieldValidator id="RequiredFieldValidator1" runat="server" ControlToValidate="tbVoornaam" ErrorMessage="voornaam is a required field" ForeColor="Red"></asp:RequiredFieldValidator>
+                <asp:TextBox ID="tbtussenvoegsel" runat="server" Class="input" placeholder="tussenvoegsel"></asp:TextBox>
+                <asp:TextBox ID="tbachternaam" runat="server" Class="input" placeholder="achternaam"></asp:TextBox>
+                                <asp:RequiredFieldValidator id="RequiredFieldValidator3" runat="server" ControlToValidate="tbachternaam" ErrorMessage="achternaam is a required field" ForeColor="Red"></asp:RequiredFieldValidator>
+
+                <asp:TextBox ID="tbstraat" runat="server" Class="input" placeholder="straat"></asp:TextBox>
+                                <asp:RequiredFieldValidator id="RequiredFieldValidator4" runat="server" ControlToValidate="tbstraat" ErrorMessage="straat is a required field" ForeColor="Red"></asp:RequiredFieldValidator>
+
+                <asp:TextBox ID="tbhuisnr" runat="server" Class="input" placeholder="huisnummer"></asp:TextBox>
+                                <asp:RequiredFieldValidator id="RequiredFieldValidator5" runat="server" ControlToValidate="tbhuisnr" ErrorMessage="huisnr is a required field" ForeColor="Red"></asp:RequiredFieldValidator>
+
+                <asp:TextBox ID="tbwoonplaats" runat="server" Class="input" placeholder="postcode"></asp:TextBox>
+                                <asp:RequiredFieldValidator id="RequiredFieldValidator6" runat="server" ControlToValidate="tbwoonplaats" ErrorMessage="postcode is a required field" ForeColor="Red"></asp:RequiredFieldValidator>
+
+                <asp:TextBox ID="tbbanknr" runat="server" Class="input" placeholder="banknr"></asp:TextBox>
+                                <asp:RequiredFieldValidator id="RequiredFieldValidator7" runat="server" ControlToValidate="tbbanknr" ErrorMessage="banknr is a required field" ForeColor="Red"></asp:RequiredFieldValidator>
+
+                <asp:Button ID="btnSignup" runat="server" Class="button" Text="Sign up" OnClick="btnSignup_OnClick"/>
+            </div>
+
+    </div>
+    </form>
+</body>
+</html>
