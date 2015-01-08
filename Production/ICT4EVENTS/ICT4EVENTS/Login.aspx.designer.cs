3e58220d6dc5947e2b376996f7f3a463a80bd17c
Dennisoost <d.oosterling@student.fontys.nl>
2015-01-08 13:56:56 +0100

    Added label to login

--- Production/ICT4EVENTS/ICT4EVENTS/Login.aspx.designer.cs
+++ Production/ICT4EVENTS/ICT4EVENTS/Login.aspx.designer.cs
@@ -67,6 +67,15 @@ namespace ICT4EVENTS {
         protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
         
         /// <summary>
+        /// lblLogin control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblLogin;
+        
+        /// <summary>
         /// btnLogin control.
         /// </summary>
         /// <remarks>
