39db1f25d1e6e43472d027384bd41691358cc271
markmjb <m.blesgraaf@student.fontys.nl>
2015-01-08 21:39:23 +0100

    Code crashed omdat admin geen reservering heeft atm, In comments gezet.

--- Production/ICT4EVENTS/ICT4EVENTS/Home.aspx.cs
+++ Production/ICT4EVENTS/ICT4EVENTS/Home.aspx.cs
@@ -20,41 +20,41 @@ namespace ICT4EVENTS
             }
             else
             {*/
-                FillListBoxes();
+                //FillListBoxes();
            // }
        
         }
 
-        public void FillListBoxes()
-        {
-            string username = "admin";
-            
-            ReserverationInfoListBox.Items.Clear();
+        //public void FillListBoxes()
+        //{
+        //    string username = "admin";
 
-            List<string> reservationList = new List<string>();
+        //    ReserverationInfoListBox.Items.Clear();
 
-            reservationList = B.ReservationInfo(username);
-         
-                string ResID = "ReserveringsID:      " + reservationList.ElementAt(0);
-                string Personen = "AccountID:       " +  reservationList.ElementAt(1);
-                string start = "Aankomstdatum reservering:        " + reservationList.ElementAt(2);
-                string stop = "Einddatum reservering:        " + reservationList.ElementAt(3);
-                string bestaald = reservationList.ElementAt(4); 
+        //    List<string> reservationList = new List<string>();
 
-                ReserverationInfoListBox.Items.Add(ResID);
-                ReserverationInfoListBox.Items.Add(Personen);
-                ReserverationInfoListBox.Items.Add(start);
-                ReserverationInfoListBox.Items.Add(stop);
-                ReserverationInfoListBox.Items.Add(bestaald);
+        //    reservationList = B.ReservationInfo(username);
 
-            string Kampeer = "Kampeerplaatsen:  ";
-            for (int i = 0; i < B.CampNumbersInfo(username).Count; i++)
-            {
-                Kampeer += B.CampNumbersInfo(username).ElementAt(i) + ",";
-            }
+        //    string ResID = "ReserveringsID:      " + reservationList.ElementAt(0);
+        //    string Personen = "AccountID:       " + reservationList.ElementAt(1);
+        //    string start = "Aankomstdatum reservering:        " + reservationList.ElementAt(2);
+        //    string stop = "Einddatum reservering:        " + reservationList.ElementAt(3);
+        //    string bestaald = reservationList.ElementAt(4);
 
-            ReserverationInfoListBox.Items.Add(Kampeer);
+        //    ReserverationInfoListBox.Items.Add(ResID);
+        //    ReserverationInfoListBox.Items.Add(Personen);
+        //    ReserverationInfoListBox.Items.Add(start);
+        //    ReserverationInfoListBox.Items.Add(stop);
+        //    ReserverationInfoListBox.Items.Add(bestaald);
 
-        }
+        //    string Kampeer = "Kampeerplaatsen:  ";
+        //    for (int i = 0; i < B.CampNumbersInfo(username).Count; i++)
+        //    {
+        //        Kampeer += B.CampNumbersInfo(username).ElementAt(i) + ",";
+        //    }
+
+        //    ReserverationInfoListBox.Items.Add(Kampeer);
+
+        //}
     }
 }
