7cd6e9a47de760c4231abd12b76e7327aeaf547b
markmjb <m.blesgraaf@student.fontys.nl>
2015-01-09 12:27:16 +0100

    k

--- Production/ICT4EVENTS/Businesslayer/business.cs
+++ Production/ICT4EVENTS/Businesslayer/business.cs
@@ -37,9 +37,9 @@ namespace Businesslayer
         {
             return _databaseConnection.CheckBarcode(barcode);
         }
-        public string Getcategory(int id)
+        public List<string> Getcategory()
         {
-            return this._databaseConnection.Getcategory(id);
+            return this._databaseConnection.Getcategory();
         }
 
         public List<string> UserInfo(string user)
@@ -59,7 +59,7 @@ namespace Businesslayer
 
        public List<string> ReserveditemsList(string username)
        {
-          return _databaseConnection.ReservedItemsList(username);
+        return _databaseConnection.ReservedItemsList(username);
        }
 
        public void LeasedItemViews(ListView LV)
